dbc719afb3f1c811c3c1596772828774a3ee77bb
Matheus Madeira <matheus.madeira@townsq.com.br>
2022-03-12 19:18:54 -0300

    Update server and client files to use unix line delimiters standart

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -15,59 +15,59 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int socket_descriptor, number_of_bytes;
-	unsigned int size_of_message = sizeof(struct sockaddr_in);
-	struct sockaddr_in server_address, from;
-	struct hostent *server;
+  int socket_descriptor, number_of_bytes;
+  unsigned int size_of_message = sizeof(struct sockaddr_in);
+  struct sockaddr_in server_address, from;
+  struct hostent *server;
 
-	char buffer[BUFFER_SIZE];
-	if (argc < 2)
-	{
-		fprintf(stderr, "usage %s hostname\n", argv[0]);
-		exit(0);
-	}
+  char buffer[BUFFER_SIZE];
+  if (argc < 2)
+  {
+    fprintf(stderr, "usage %s hostname\n", argv[0]);
+    exit(0);
+  }
 
-	server = gethostbyname(argv[1]);
-	if (server == NULL)
-	{
-		fprintf(stderr, "ERROR, no such host\n");
-		exit(0);
-	}
+  server = gethostbyname(argv[1]);
+  if (server == NULL)
+  {
+    fprintf(stderr, "ERROR, no such host\n");
+    exit(0);
+  }
 
-	socket_descriptor = socket(AF_INET, SOCK_DGRAM, DEFAULT_PROTOCOL);
-	if (socket_descriptor == ERROR_VALUE)
-		printf("ERROR opening socket");
+  socket_descriptor = socket(AF_INET, SOCK_DGRAM, DEFAULT_PROTOCOL);
+  if (socket_descriptor == ERROR_VALUE)
+    printf("ERROR opening socket");
 
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(PORT);
-	server_address.sin_addr = *((struct in_addr *)server->h_addr);
-	bzero(&(server_address.sin_zero), sizeof(server_address.sin_zero));
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(PORT);
+  server_address.sin_addr = *((struct in_addr *)server->h_addr);
+  bzero(&(server_address.sin_zero), sizeof(server_address.sin_zero));
 
-	printf("Enter the message: ");
-	bzero(buffer, BUFFER_SIZE);
-	fgets(buffer, BUFFER_SIZE, stdin);
+  printf("Enter the message: ");
+  bzero(buffer, BUFFER_SIZE);
+  fgets(buffer, BUFFER_SIZE, stdin);
 
-	number_of_bytes = sendto(
-		socket_descriptor,
-		buffer,
-		strlen(buffer),
-		NONE,
-		(const struct sockaddr *)&server_address,
-		size_of_message);
-	if (number_of_bytes < 0)
-		printf("ERROR sendto");
+  number_of_bytes = sendto(
+      socket_descriptor,
+      buffer,
+      strlen(buffer),
+      NONE,
+      (const struct sockaddr *)&server_address,
+      size_of_message);
+  if (number_of_bytes < 0)
+    printf("ERROR sendto");
 
-	number_of_bytes = recvfrom(
-		socket_descriptor, buffer,
-		BUFFER_SIZE,
-		NONE,
-		(struct sockaddr *)&from,
-		&size_of_message);
-	if (number_of_bytes < 0)
-		printf("ERROR recvfrom");
+  number_of_bytes = recvfrom(
+      socket_descriptor, buffer,
+      BUFFER_SIZE,
+      NONE,
+      (struct sockaddr *)&from,
+      &size_of_message);
+  if (number_of_bytes < 0)
+    printf("ERROR recvfrom");
 
-	printf("Got an ack: %s\n", buffer);
+  printf("Got an ack: %s\n", buffer);
 
-	close(socket_descriptor);
-	return 0;
+  close(socket_descriptor);
+  return 0;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -14,50 +14,50 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int socket_descriptor, number_of_bytes;
-	socklen_t client_address_struct_size;
-	struct sockaddr_in server_address, client_address;
-	char buffer[BUFFER_SIZE];
+  int socket_descriptor, number_of_bytes;
+  socklen_t client_address_struct_size;
+  struct sockaddr_in server_address, client_address;
+  char buffer[BUFFER_SIZE];
 
-	socket_descriptor = socket(AF_INET, SOCK_DGRAM, DEFAULT_PROTOCOL);
-	if (socket_descriptor == ERROR_VALUE)
-		printf("ERROR opening socket");
+  socket_descriptor = socket(AF_INET, SOCK_DGRAM, DEFAULT_PROTOCOL);
+  if (socket_descriptor == ERROR_VALUE)
+    printf("ERROR opening socket");
 
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(PORT);
-	server_address.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(server_address.sin_zero), sizeof(server_address.sin_zero));
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(PORT);
+  server_address.sin_addr.s_addr = INADDR_ANY;
+  bzero(&(server_address.sin_zero), sizeof(server_address.sin_zero));
 
-	if (bind(socket_descriptor, (struct sockaddr *)&server_address, sizeof(struct sockaddr)) < 0)
-		printf("ERROR on binding");
+  if (bind(socket_descriptor, (struct sockaddr *)&server_address, sizeof(struct sockaddr)) < 0)
+    printf("ERROR on binding");
 
-	client_address_struct_size = sizeof(struct sockaddr_in);
+  client_address_struct_size = sizeof(struct sockaddr_in);
 
-	while (TRUE)
-	{
-		/* receive from socket */
-		number_of_bytes = recvfrom(
-			socket_descriptor,
-			buffer,
-			BUFFER_SIZE,
-			NONE,
-			(struct sockaddr *)&client_address,
-			&client_address_struct_size);
-		if (number_of_bytes < 0)
-			printf("ERROR on recvfrom");
-		printf("Received a datagram: %s\n", buffer);
+  while (TRUE)
+  {
+    /* receive from socket */
+    number_of_bytes = recvfrom(
+        socket_descriptor,
+        buffer,
+        BUFFER_SIZE,
+        NONE,
+        (struct sockaddr *)&client_address,
+        &client_address_struct_size);
+    if (number_of_bytes < 0)
+      printf("ERROR on recvfrom");
+    printf("Received a datagram: %s\n", buffer);
 
-		/* send to socket */
-		number_of_bytes = sendto(
-			socket_descriptor,
-			"Got your message\n",
-			17, // Size of message
-			NONE,
-			(struct sockaddr *)&client_address, sizeof(struct sockaddr));
-		if (number_of_bytes < 0)
-			printf("ERROR on sendto");
-	}
+    /* send to socket */
+    number_of_bytes = sendto(
+        socket_descriptor,
+        "Got your message\n",
+        17, // Size of message
+        NONE,
+        (struct sockaddr *)&client_address, sizeof(struct sockaddr));
+    if (number_of_bytes < 0)
+      printf("ERROR on sendto");
+  }
 
-	close(socket_descriptor);
-	return 0;
+  close(socket_descriptor);
+  return 0;
 }
